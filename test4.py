480610aa82d0d30fb4c0a87ffac1d9b1a2de45f0
Alex95976 <alex95.mgn@gmail.com>
2026-08-08 14:27:52 +0800

    Add files via upload

--- test4.py
+++ test4.py
@@ -1,4 +1,4 @@
-def get_trade_conditions(ohlc_data, macd_data, rsi data):
+def get_trade_conditions(ohlc_data, macd_data, rsi_data):
     """
     OHLC датаг ашиглан 'open_up', 'open_down' хувьсагчдыг тооцоолно.
     `macd_data` параметрийг одоогоор ашиглахгүй.
@@ -18,8 +18,10 @@ def get_trade_conditions(ohlc_data, macd_data, rsi data):
         # `open_trend`-г тооцоолох
         if open0 > open1:
             result["open_up"] = True
+            result["open_down"] = False
         elif open0 < open1:
             result["open_down"] = True
+            result["open_up"] = False
         # Хэрэв open0 == open1 бол хоёулаа False хэвээр үлдэнэ.
 
     except (KeyError, IndexError, TypeError) as e:
