837cae024ad325012494e343876bb109172c1bc2
Alex95976 <alex95.mgn@gmail.com>
2026-08-08 14:21:19 +0800

    Add files via upload

---
+++ test4.py
@@ -0,0 +1,29 @@
+def get_trade_conditions(ohlc_data, macd_data, rsi data):
+    """
+    OHLC датаг ашиглан 'open_up', 'open_down' хувьсагчдыг тооцоолно.
+    `macd_data` параметрийг одоогоор ашиглахгүй.
+    """
+    # Анхны утгуудыг тодорхойлох
+    result = {
+        "open_up": False,
+        "open_down": False,
+        "error": None
+    }
+
+    try:
+        # Шаардлагатай хувьсагчдыг задлах
+        open0 = ohlc_data["ohlc_list"][0]["open"]
+        open1 = ohlc_data["ohlc_list"][1]["open"]
+
+        # `open_trend`-г тооцоолох
+        if open0 > open1:
+            result["open_up"] = True
+        elif open0 < open1:
+            result["open_down"] = True
+        # Хэрэв open0 == open1 бол хоёулаа False хэвээр үлдэнэ.
+
+    except (KeyError, IndexError, TypeError) as e:
+        # Дата дутуу (жишээ нь: ohlc_list байхгүй) үед алдааг барьж авах
+        result["error"] = f"Data structure error while processing OHLC: {e}"
+
+    return result
